c2f646ba5d6bdc5023b57c3935d47984a909fce1
RuchitDonda <ruchitdonda64@gmail.com>
2026-05-30 10:42:31 +0000

    datatype

---
+++ 01_basics/1_variable.js
@@ -0,0 +1,11 @@
+const accountid=123  //never change value
+let accountemail="ruchit@gmail.com" //declare varibale-1
+var accountpass="12345" //declare varibale-2
+accountcity='surat' //not use like that
+let state;
+console.table([accountid,accountemail,accountpass,accountcity,state]);  //print whole thing inform of table
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+ */
---
+++ 01_basics/2_datatype.js
@@ -0,0 +1,18 @@
+"use strict"//treat all js code as newer version
+
+// alert("hello")//we are using nodejs ,not browser
+
+
+//number=>2 to power 53
+//bigint
+//string=>""
+//boolean=t/f
+//null=standalone value
+//undefined
+//symbol=unique
+
+console.log(typeof 18);
+console.log(typeof null);  //interview ask problme
+console.log(typeof undefined);
+
+
